1278ca2e683980e713412b4bea9c2e1746d7c17b
Kevin Mateo Gomez <gomezdiezkevinmateo@gmail.com>
2025-05-08 16:29:50 -0500

    eliminacion y cambios de nombre

--- sql/crear-usuarios.sql
+++
@@ -1,7 +0,0 @@
-create table Usuarios (
-    cedula integer primary key,
-    nombre text not null,
-    base_settlement_income real not null,
-    current_legal_minimum_wage real not null,
-    pension_porcentage real not null
-);
