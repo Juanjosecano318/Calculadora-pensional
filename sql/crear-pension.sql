b4de07b4ca1a3a56cc2f53c383cb52bdb30098dc
Kevin Mateo Gomez <165510156+KevinMateoG@users.noreply.github.com>
2025-05-08 19:34:08 -0500

    Delete sql/crear-pension.sql

--- sql/crear-pension.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists Pensiones (
-        cedula integer primary key,
-        base_settlement_income real not null,
-        current_legal_minimum_wage real not null,
-        pension_porcentage real not null
-        );
