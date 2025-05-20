8ef13a60100b98aa237b78da0a7b80db865af2dd
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2025-05-20 09:16:03 -0500

    implementacion_actual

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def Home():
+    return render_template("index.html")
+
+@app.route('/buscar')
+def buscar():
+    return render_template("buscar.html")
+
+@app.route('/resultado_busqueda')
+def resultado_busqueda():
+    return render_template('resultado_busqueda.html', cedula=request.args["cedula"])
+
+# Puedes agregar aquí todas las demás rutas que ya tienes
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
